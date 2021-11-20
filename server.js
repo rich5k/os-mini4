2172aaf1d59e1332972e25d50067ada61816f23e
rich5k <richardanatsui@gmail.com>
2021-11-20 13:54:29 +0000

    testing api endpoint

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/api",(req,res)=>{
+    res.json(["The", "test", "is", "awesome"]);
+});
+
 app.listen(3000, ()=>{
     console.log("Server running on port 3000");
 })
