ad158af09b3d3deaec4dcc586303881a13b37c11
rich5k <richardanatsui@gmail.com>
2021-11-22 11:37:27 +0000

    created permissions controller and modified server file

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port= 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/api",(req,res)=>{
     res.json(["The", "test", "is", "awesome"]);
 });
 
-app.listen(3000, ()=>{
-    console.log("Server running on port 3000");
+app.listen(port, ()=>{
+    console.log(`Server running on port ${port}`);
 })
