d452c99bc409cd38729ed6c2c53885254e27ccdf
rich5k <richardanatsui@gmail.com>
2021-11-24 11:30:57 +0000

    modified getParityBits function

--- api/controllers/permissions-ctrl.js
+++ api/controllers/permissions-ctrl.js
@@ -92,7 +92,13 @@ const getParityBits= async(req,res)=>{
     var b2= req.params.bits2;
     var b3= req.params.bits3;
     var b4= req.params.bits4;
-    
+    var parityBits;
+    if(b1.length!=b2.length!=b3.length!=b4.length){
+        return res.status(404).json({
+            success: false,
+            error: "The length of one or more bits do not match the others "
+        })
+    }
     
     return res.status(200).json({
         parityBits
