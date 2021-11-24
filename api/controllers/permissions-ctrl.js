544e13d33348bf5412eabc020cafb31d3cf532ae
rich5k <richardanatsui@gmail.com>
2021-11-24 13:27:47 +0000

    added parity controller

--- api/controllers/permissions-ctrl.js
+++ api/controllers/permissions-ctrl.js
@@ -87,27 +87,7 @@ const displayPermissions= async(req,res)=>{
     })
 }
 
-const getParityBits= async(req,res)=>{
-    var b1= req.params.bits1;
-    var b2= req.params.bits2;
-    var b3= req.params.bits3;
-    var b4= req.params.bits4;
-    var parityBits;
-    if(b1.length!=b2.length!=b3.length!=b4.length){
-        return res.status(404).json({
-            success: false,
-            error: "The length of one or more bits do not match the others "
-        })
-    }
-    
-    return res.status(200).json({
-        parityBits
-
-        
-    })
-}
 
 module.exports={
-    displayPermissions,
-    getParityBits
+    displayPermissions
 }
